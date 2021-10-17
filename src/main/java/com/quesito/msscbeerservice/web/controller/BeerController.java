3bf8fbca8b9b346494cd57e5df10bb44636ee6a0
pablo <pablocuri2015@hotmail.com>
2021-10-17 17:56:11 -0500

    beer controller

---
+++ src/main/java/com/quesito/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,30 @@
+package com.quesito.msscbeerservice.web.controller;
+
+
+import com.quesito.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId) {
+        return new ResponseEntity<BeerDto>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById  (@RequestBody BeerDto beerDto,@PathVariable(name = "beerId") UUID beerId) {
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+
+}
---
+++ src/main/java/com/quesito/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.quesito.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER,PILSENER
+}
